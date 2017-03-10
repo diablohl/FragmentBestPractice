40bca19ded6f12b2971cf9c8d8ce8943cc1dff45
daiblo <hldiablo@hotmail.com>
2017-03-10 16:50:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hele/fragmentbestpractice/News.java
@@ -0,0 +1,27 @@
+package com.example.hele.fragmentbestpractice;
+
+/**
+ * Created by hele on 2017/3/8.
+ */
+
+public class News {
+    private String title;
+
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
